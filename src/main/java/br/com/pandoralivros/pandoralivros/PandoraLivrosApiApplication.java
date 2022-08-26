7ec3e44cbb3473267e97a04726ff212799669de1
Luana Kunzler <luanakdelima@gmail.com>
2022-08-26 20:02:11 -0300

    first commit

---
+++ src/main/java/br/com/pandoralivros/pandoralivros/PandoraLivrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pandoralivros.pandoralivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraLivrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandoraLivrosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pandoralivros/pandoralivros/PandoraLivrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pandoralivros.pandoralivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandoraLivrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
